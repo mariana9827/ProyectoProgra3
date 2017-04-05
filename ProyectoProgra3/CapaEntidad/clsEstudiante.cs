1478eea85bf919dd568514ec5b0bbe343fe5c175
mariana9827 <nanaga98@hotmail.com>
2017-04-04 21:25:53 -0600

    cambios

--- ProyectoProgra3/CapaEntidad/clsEstudiante.cs
+++ ProyectoProgra3/CapaEntidad/clsEstudiante.cs
@@ -12,10 +12,29 @@ namespace ProyectoProgra3.CapaEntidad
         private String Apellido2;
         private int carnet;
         private int cedula;
+        private int idDistrito;
+        private String DistritoDescripcion;
+        private int idCanton;
+        private String CantonDescripcion;
+        private int idProvincia;
+        private String ProvinciaDescripcion;
+        private int IdDireccion;
+        private String DireccionExacta;
+        private int tipoCorreo;
+        private String CorreoDescripcion;
+        private String Correo;
+        private String Estado;
+
+
+
+
+
 
         public clsEstudiante () { }
 
-        public clsEstudiante (String Nombre, String Apellido1, String Apellido2, int carnet, int cedula)
+        public clsEstudiante (String Nombre, String Apellido1, String Apellido2, int carnet, int cedula
+            , int idDistrito, String DistritoDescripcion, int idCanton, String CantonDescripcion, int idProvincia, String ProvinciaDescripcion,
+            int tipoCorreo, String CorreoDescripcion, String Correo, String Estado)
         {
             this.Nombre = Nombre;
             this.cedula = cedula;
---
+++ ProyectoProgra3/ControladoraBD/clsEstudiante.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoProgra3.ControladoraBD
+{
+    public class clsEstudiante
+    {
+        public bool insertarEstudiante (CapaEntidad.clsEstudiante Estudiante)
+        {
+            SqlCommand command = new SqlCommand();
+            command.CommandType = CommandType.Text;
+            command.CommandText = "INSERT INTO dbo.Estudiante(Carnet,IDDireccion,IDTipoCorreo,Nombre,Apellido1,Apellido2,Cedula)VALUES(@Carnet,@IDDireccion,@Nombre,@Apellido1,@Apellido2,@Cedula)";
+            command.Parameters.Add("@Carnet", SqlDbType.Int).Value = Estudiante.propCarnet;
+            command.Parameters.Add("@IDDireccion", ) =;
+           
+
+            //return new Controladora().ejecutar(command);
+        }
+
+        //public DataTable buscarRerva ()
+        //{
+        //    SqlCommand command = new SqlCommand("SELECT tipo_reserva as [Tipo de reserva], fecha_reserva as Fecha, nombre_cliente as Cliente, cedula_cliente as Cedula FROM reserva WHERE num_reserva = 1");
+        //    return new Controladora().ejecutarSelect(command);
+        //}
+
+
+        //public DataTable verReservas ()
+        //{
+        //    SqlCommand command = new SqlCommand("SELECT tipo_reserva as [Tipo de reserva], fecha_reserva as Fecha, nombre_cliente as Cliente, cedula_cliente as Cedula FROM reserva");
+        //    return new Controladora().ejecutarSelect(command);
+        //}
+    }
+}
